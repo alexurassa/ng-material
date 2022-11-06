40ce7e3f4ea60f7e24b32ca90fa8b16539c54702
alexurassa <alexurassa03@gmail.com>
2022-11-06 11:40:57 +0300

    .

---
+++ src/app/modules/material-demo/components/mat-dialog-template/mat-dialog-template.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-dialog-template',
+  templateUrl: './mat-dialog-template.component.html',
+  styles: [
+  ]
+})
+export class MatDialogTemplateComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/modules/material-demo/components/mat-dialog/mat-dialog.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-mat-dialog',
+  templateUrl: './mat-dialog.component.html',
+  styles: [
+  ]
+})
+export class MatDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
