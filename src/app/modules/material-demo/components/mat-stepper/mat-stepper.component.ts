3b7f3cfaf14ca589de0121c113277655dfd3f7d1
alexurassa <alexurassa03@gmail.com>
2022-11-10 09:07:44 +0300

    Modified stepper

--- src/app/modules/material-demo/components/mat-stepper/mat-stepper.component.ts
+++ src/app/modules/material-demo/components/mat-stepper/mat-stepper.component.ts
@@ -1,5 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+
+interface User {
+  username: string;
+  city: string;
+}
+
+
+const usersList: Array<User> = [
+  { username: 'omari', city: 'one'},
+  { username: 'jj', city: 'my city'},
+  { username: 'rashid', city: 'city two'},
+  { username: 'liv22', city: '920 city'},
+  { username: 'my username', city: 'new city'}
+]
+
 @Component({
   selector: 'app-mat-stepper',
   templateUrl: './mat-stepper.component.html',
@@ -8,6 +23,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MatStepperComponent implements OnInit {
 
+  public users: User[] = usersList
+  
   constructor() { }
 
   ngOnInit(): void {
