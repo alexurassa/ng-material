58fecf61bac57cfd418d3555008309d77ba966ad
alexurassa <alexurassa03@gmail.com>
2022-11-11 01:47:18 +0300

    Modified snackbar

--- src/app/modules/material-demo/components/mat-snackbar/mat-snackbar.component.ts
+++ src/app/modules/material-demo/components/mat-snackbar/mat-snackbar.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatSnackBar, MatSnackBarRef, MAT_SNACK_BAR_DATA, TextOnlySnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-mat-snackbar',
@@ -10,8 +10,9 @@ import { MatSnackBar, MatSnackBarRef } from '@angular/material/snack-bar';
 export class MatSnackbarComponent implements OnInit {
 
   public openSnackBar(message: string, action: string): void {
-    const snackbarRef: MatSnackBarRef<any> = this.snackBar.open(
-      message, action, { duration: 2000 })
+    const snackbarRef: MatSnackBarRef<TextOnlySnackBar> = this.snackBar.open(
+      message, action, { 
+        duration: 2000, horizontalPosition: 'right', verticalPosition: 'top'})
 
     snackbarRef.onAction().subscribe(() => {
       console.log("SnackBar was triggered")
@@ -25,7 +26,8 @@ export class MatSnackbarComponent implements OnInit {
   }
 
   public openCustomSnackBarComponent(): void {
-    this.snackBar.openFromComponent(CustomSnackBarComponent, { duration: 2000 })
+    this.snackBar.openFromComponent(CustomSnackBarComponent, 
+      { duration: 2000, data: { username: 'Peterson'} })
   }
 
   ngOnInit(): void {
@@ -38,9 +40,11 @@ export class MatSnackbarComponent implements OnInit {
 @Component({
   selector: 'app-custom-snackbar',
   template: `
-  <span class="mat-headline">custom component</span>
+  <span class="mat-body-2">Hi {{ data.username ? data.username + ', This is' : ',' }} custom component</span>
   `
 })
 export class CustomSnackBarComponent {
 
+
+  public constructor(@Inject(MAT_SNACK_BAR_DATA) public data: { username: string | undefined }) {}
 }
