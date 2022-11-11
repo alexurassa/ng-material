cd972fde954bca3072642df603d5d59a569bd729
alexurassa <alexurassa03@gmail.com>
2022-11-11 23:38:22 +0300

    date-range picker implemented

--- src/app/modules/material-demo/components/mat-datepicker/mat-datepicker.component.ts
+++ src/app/modules/material-demo/components/mat-datepicker/mat-datepicker.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-mat-datepicker',
   templateUrl: './mat-datepicker.component.html',
-  styles: [
-  ]
+  styleUrls: ['./mat-datepicker.component.css']
 })
 export class MatDatepickerComponent implements OnInit {
 
   minDate: Date = new Date()
   maxDate: Date = new Date(2022, 10, 20)
+
+  groupOne: FormGroup = new FormGroup({
+    start: new FormControl(new Date()),
+    end: new FormControl(new Date())
+  })
+
+  groupTwo: FormGroup = new FormGroup({
+    start: new FormControl(new Date()),
+    end: new FormControl(new Date())
+  })
 
   // dateFilter = (date: Date | null) => {
   //   const day: number = date.getDay()
