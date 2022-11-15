8936645cc43cd499a113546d3091f56f95cb5356
alexurassa <alexurassa03@gmail.com>
2022-11-15 21:46:25 +0300

    more about progress-spinners

--- src/app/modules/material-demo/components/mat-progress-spinner/mat-progress-spinner.component.ts
+++ src/app/modules/material-demo/components/mat-progress-spinner/mat-progress-spinner.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemePalette } from '@angular/material/core';
+import { ProgressSpinnerMode } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-mat-progress-spinner',
@@ -9,12 +11,18 @@ import { Component, OnInit } from '@angular/core';
 export class MatProgressSpinnerComponent implements OnInit {
 
   progressValue: number = 10;
+  value: number = 5;
+  progressMode: ProgressSpinnerMode = 'determinate'
+  colorTheme: ThemePalette = 'warn'
 
   updateProgressValue(): void {
     setTimeout(() => {
       this.progressValue = this.progressValue + 5
     }, 1000)
   }
+
+  materialColors: string[] = ['accent', 'primary', 'warn']
+  color: string = 'primary'
 
   constructor() { }
 
