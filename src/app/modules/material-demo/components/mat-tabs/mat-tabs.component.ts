b9d8770388a1a87133972675ece6c239b14f94c5
alexurassa <alexurassa03@gmail.com>
2022-11-19 23:22:19 +0300

    Tabs implemented

--- src/app/modules/material-demo/components/mat-tabs/mat-tabs.component.ts
+++ src/app/modules/material-demo/components/mat-tabs/mat-tabs.component.ts
@@ -1,8 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { ThemePalette } from '@angular/material/core';
 import { MatTabLink } from '@angular/material/tabs';
 import { Observable, Observer } from 'rxjs';
 
 interface AsyncTab {
+  label: string;
+  content: string;
+}
+
+interface BasicTabItem {
   label: string;
   content: string;
 }
@@ -16,10 +23,43 @@ export class MatTabsComponent implements OnInit {
 
   links: string[] = ['home', 'about', 'services', 'activities']
   activeLink: string = 'home';
+  materialThemes: ThemePalette[] = ['primary', 'accent', 'warn']
 
   asyncTabs!: Observable<AsyncTab[]>;
 
+  tabItems: BasicTabItem[] = [
+    {label: 'First', content: 'Content - 1'},
+    {label: 'Second', content: 'Content - 2'},
+    {label: 'Third', content: 'Content - 3'},
+  ]
+
+  lotsOfTabs: Array<BasicTabItem> = new Array(25).fill(0).map(
+    (item: BasicTabItem, index) => {
+      return { label: `Tab ${index}`, content: `Content ${index}`}
+    })
+
+  tabs: string[] = ['First', 'Second', 'Third']
+  activeTabIndex: FormControl = new FormControl<number>(0)
+
+  public addNewTab(addWithActiveStatus: boolean): void {
+    // add first 
+    this.tabs.push("New tab")
+
+    // make selected if active checkbox is clicked
+    if(addWithActiveStatus) {
+      this.activeTabIndex.setValue(this.tabs.length -1)
+    }
+  }
+
+  public removeTab(index: number): void {
+    this.tabs.splice(index, 1)
+  }
+
   constructor() { 
+  }
+
+
+  ngOnInit(): void {
     this.asyncTabs = new Observable<AsyncTab[]>((observer: Observer<AsyncTab[]>) => {
       setTimeout(() => {
         observer.next([
@@ -27,11 +67,8 @@ export class MatTabsComponent implements OnInit {
           { label: 'Second', content: 'Content 2'},
           { label: 'Last', content: 'Content 3'},
         ])
-      }, 3000)
+      }, 2000)
     })
-  }
-
-  ngOnInit(): void {
   }
 
 }
