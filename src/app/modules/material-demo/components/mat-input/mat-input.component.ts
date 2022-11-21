9ad9ac231a6d8f3f732ecee3425b9ba6acdb3df1
alexurassa <alexurassa03@gmail.com>
2022-11-21 17:28:18 +0300

    Deep dive into form Inputs

--- src/app/modules/material-demo/components/mat-input/mat-input.component.ts
+++ src/app/modules/material-demo/components/mat-input/mat-input.component.ts
@@ -1,16 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import {
+  NgForm,
+  FormGroupDirective,
+  FormControl,
+  Validators,
+} from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+
+/**
+ * @usageNotes
+ * Custom ErrorStateMatcher that returns the invalid control when touched or dirty
+ */
+class CustomErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(
+    formControl: FormControl,
+    form: FormGroupDirective | NgForm | null
+  ): boolean {
+    const submitted: boolean | null = form && form.submitted;
+
+    return !!(
+      formControl &&
+      formControl.invalid &&
+      (formControl.touched || formControl.dirty || submitted)
+    );
+  }
+}
 
 @Component({
   selector: 'app-mat-input',
   templateUrl: './mat-input.component.html',
-  styles: [
-  ]
+  styleUrls: ['./mat-input.component.css']
 })
 export class MatInputComponent implements OnInit {
 
-  constructor() { }
+  public customErrorStateMatcher: CustomErrorStateMatcher = new CustomErrorStateMatcher();
 
-  ngOnInit(): void {
-  }
+  emailControl: FormControl<string | null> = new FormControl<string>('', [Validators.required, Validators.email]);
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
